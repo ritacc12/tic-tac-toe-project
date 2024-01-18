63d5130b7241b156bf46298a13630515d79a0517
ritacc12 <rita20920@gmail.com>
2024-01-18 17:55:22 +0800

    add onRestart props for pass the ReStart function

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const GameOver = ({ winner }) => {
+const GameOver = ({ winner, onRestart }) => {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It's a draw!</p>}
       <p>
-        <button>Rematch!</button>
+        <button onClick={onRestart}>Rematch!</button>
       </p>
     </div>
   );
